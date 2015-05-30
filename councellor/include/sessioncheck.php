83c958b17691a6fa16cd71750a8e00bd448a6124
kiran <askiran123@gmail.com>
2015-05-31 01:28:34 +0530

    deleted councellor

--- councellor/include/sessioncheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../application/conn.php');
-if($_SESSION['idcouncellor']=='')
-{
-  echo "<script>alert('Your Session has expired, Please login again');</script>";
-  echo "<script>parent.location='index.php'</script>";
-  exit;
-}
-?>
